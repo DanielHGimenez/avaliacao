72156786fcc4b5529089ad3b6a18dd3f271ae8c2
Daniel Hermenegildo Gimenez <danielhgimenez027@gmail.com>
2021-12-21 22:44:36 -0300

    Setup projeto back

---
+++ back/src/main/java/dhgimenez/agendador_transacoes/AgendadorTransacoesApplication.java
@@ -0,0 +1,13 @@
+package dhgimenez.agendador_transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTransacoesApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/dhgimenez/agendador_transacoes/AgendadorTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package dhgimenez.agendador_transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
